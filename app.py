b6aadea906d9d8a6eba82111927ef3a0e47f5c35
Derek Teed <Derek@MacBook-Air.local>
2020-07-18 11:00:05 -0500

    update

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL") or 'postgres://postgres:postgres@localhost/flask-hw'
+print(app.config['SQLALCHEMY_DATABASE_URI'])
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+    #return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run()
+
+
+# @app.route("/api/whatever")
+# def my_func():
+#     # call databse with criteria
+
+#     return render_template("derek/index.html")
+# #     #return json data
+# #     return jsonify(data)
+
+#have to use pythondata 3.6.10 on bottom left of VSC
+
+
+
