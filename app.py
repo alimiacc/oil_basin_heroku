1d5559e491295386ca32e6edcf4d52f5ae058691
Derek Teed <Derek@MacBook-Air.local>
2020-07-18 13:16:45 -0500

    update

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
-    #return render_template("index.html")
+    #return 'Hello World!'
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
